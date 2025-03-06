d6b85ab438b67ddf087ad11cc68ba47771a1ac82
interinodwayne <179785793+interinodwayne@users.noreply.github.com>
2025-03-06 15:12:46 +0800

    added a an element and a get position function

---
+++ position.js
@@ -0,0 +1,6 @@
+function getPosition(element){
+    var position = element.getBoundingClientRect();
+    var x = position.left;
+    var y = position.top;
+}
+let element = document.getElementById();
