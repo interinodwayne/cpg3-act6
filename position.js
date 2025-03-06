b7e9be822c9a2c5e0f693a780a1a057959e006db
interinodwayne <179785793+interinodwayne@users.noreply.github.com>
2025-03-06 16:19:38 +0800

    finished by added function calling and other div

--- position.js
+++ position.js
@@ -2,5 +2,12 @@ function getPosition(element){
     var position = element.getBoundingClientRect();
     var x = position.left;
     var y = position.top;
+    console.log("X coordinates:" + x);
+    console.log("Y coordinates:" + y);
 }
-let element = document.getElementById();
+header = document.getElementById("header");
+div = document.getElementById("div");
+console.log("Header positions");
+getPosition(header);
+console.log("div positions");
+getPosition(div);
