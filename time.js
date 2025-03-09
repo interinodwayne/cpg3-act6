ce60786f866d1eb2560b2c1d8251706ede66edce
interinodwayne <179785793+interinodwayne@users.noreply.github.com>
2025-03-09 14:28:30 +0800

    finished js

--- time.js
+++ time.js
@@ -1,10 +1,14 @@
+dialog = document.getElementById("greet");
 function startTime(time) {
     document.getElementById('txt').innerHTML =  time.value;
     if(time.value>="00:00"&&time.value<"12:00"){
-        document.getElementById("greet").innerHTML= "Good Morning";
+        dialog.innerHTML= "Good Morning!";
     }if(time.value>"11:59"&&time.value<"18:00"){
-        document.getElementById("greet").innerHTML= "Good Afternoon";
+        dialog.innerHTML= "Good Afternoon!";
+    }if(time.value>="18:00"&&time.value<="23:59"){
+        dialog.innerHTML= "Good Evening!";
     }
+    dialog.show();
 }
   
 form = document.getElementById("form");
